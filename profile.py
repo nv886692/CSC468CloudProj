55262be829c8c3ce77039f10b23b67875ae1166f
nv886692 <78368479+nv886692@users.noreply.github.com>
2021-04-30 00:58:32 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -1,3 +1,21 @@
+# Import the Portal object.
+import geni.portal as portal
+# Import the ProtoGENI library.
+import geni.rspec.pg as rspec
+
+# Create a Request object to start building the RSpec.
+request = portal.context.makeRequestRSpec()
+ 
+# Add a raw PC to the request.
+node = request.RawPC("node")
+
+# Request that a specific image be installed on this node
+node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU20-64-STD";
+
+# Install and execute scripts on the node. 
+node.addService(rspec.Execute(shell="sh", command="sudo bash /local/repository/install_docker.sh")) # Installs docker
+portal.context.printRequestRSpec()
+'''/*
 import geni.portal as portal
 import geni.rspec.pg as pg
 import geni.rspec.igext as IG
@@ -36,3 +54,4 @@ for i in range(3):
   node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/install_docker.sh"))
   
 pc.printRequestRSpec(request)
+*'''
